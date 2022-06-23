7c3bd86d4b94244c061d0d2c51d33bbdfaa36a8e
Wisdom Egba <99222194+wiz-js@users.noreply.github.com>
2022-06-23 15:19:06 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+var num1 = parseInt(prompt('input Number: '))
+var operator = prompt('Operator: ')
+var num2 = parseInt(prompt('input Number: '))
+
+if(operator == '+'){
+    console.log(num1 + num2)
+    alert(num1 + num2)
+}
+else if(operator == '-'){
+    console.log(num1 - num2)
+    alert(num1 - num2)
+}
+else if(operator == '*'){
+    console.log(num1 * num2)
+    alert(num1 * num2)
+}
+else if(operator == '/'){
+    console.log(num1 / num2)
+    alert(num1 / num2)
+}
